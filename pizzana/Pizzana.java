7a0089e1c4d17c933f8699c7d2241aac126cbbda
Ahmed Mostafa <01029943015a@gmail.com>
2025-11-23 21:46:46 +0200

    tt

--- pizzana/Pizzana.java
+++ pizzana/Pizzana.java
@@ -1,5 +1,6 @@
 package pizzana;
 import pizzana.payments.*;
+import java.util.ArrayList;
 
 import java.util.Scanner;
 
@@ -9,6 +10,7 @@ import pizzana.observers.*;
 import pizzana.payments.*;
 
 public class Pizzana {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Pizzana!");
         Scanner scanner = new Scanner(System.in);
@@ -17,6 +19,16 @@ public class Pizzana {
         menuDisplay.showMainMenu();
         System.out.print("Please select your meal options: ");
         Integer mealOptions = scanner.nextInt();
+        ArrayList<OrderItem> orderItems = new ArrayList<>();
+
+        while (true)
+        {
+            System.out.print("Enter meal name to add to order (or type 'done' to finish): ");
+            System.out.println("DO u want to add any extra ?");
+            scanner.
+            scanner.close();
+        }
+        
 
 
     }
