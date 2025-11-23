5608a9bc439e80b98e5bedccfcd9b7064950e0ff
pxlman <agxgv7573@gmail.com>
2025-11-23 22:09:54 +0200

    [+]

--- pizzana/Pizzana.java
+++ pizzana/Pizzana.java
@@ -8,6 +8,7 @@ import pizzana.bills.*;
 import pizzana.discounts.*;
 import pizzana.observers.*;
 import pizzana.payments.*;
+import pizzana.meals.*;
 
 public class Pizzana {
 
@@ -17,19 +18,103 @@ public class Pizzana {
 
         MenuDisplaySystem menuDisplay = new MenuDisplaySystem();
         menuDisplay.showMainMenu();
-        System.out.print("Please select your meal options: ");
-        Integer mealOptions = scanner.nextInt();
-        ArrayList<OrderItem> orderItems = new ArrayList<>();
+        Order order = new Order();
 
-        while (true)
-        {
-            System.out.print("Enter meal name to add to order (or type 'done' to finish): ");
-            System.out.println("DO u want to add any extra ?");
-            scanner.
-            scanner.close();
+        // While adding new meals to order
+mealsLoop:
+        while (true){
+            // While adding extra items to a meal
+            System.out.print("Please select your meal options: ");
+            Integer mealOption = scanner.nextInt();
+            Meal selectedMeal = null;
+            if(mealOption != 1 && mealOption != 2 && mealOption != 3){
+                System.out.println("Invalid meal option. Please try again.");
+                continue;
+            }
+            switch (mealOption) {
+                case 1:
+                    selectedMeal = new Burger();
+                    break;
+                case 2:
+                    selectedMeal = new EasternPizza();
+                    break;
+                case 3:
+                    selectedMeal = new ClassicBurger();
+                    break;
+            }
+extraLoop:
+            while (true)
+            {
+                System.out.println("Do u want to add any extra (extra code or -1 for none)?");
+                Integer extraOption = scanner.nextInt();
+                if (extraOption == -1){
+                    System.out.print("Enter quantity of this meal: ");
+                    Integer quantity = scanner.nextInt();
+                    order.addMeal(selectedMeal, quantity);
+                    break;
+                } else {
+                    switch (extraOption) {
+                        case 1:
+                            selectedMeal = new ExtraCheese(selectedMeal);
+                            break;
+                        case 2:
+                            selectedMeal = new ExtraSauce(selectedMeal);
+                            break;
+                        case 3:
+                            selectedMeal = new ExtraBeetrootJam(selectedMeal);
+                            break;
+                        default:
+                            System.out.println("Invalid extra option. Please try again.");
+                            continue extraLoop;
+                    }
+                    System.out.println("Extra added. Current meal: " + selectedMeal.getName() + " - " + selectedMeal.getCost() + "$");
+                }
+            }
+            while (true){
+                System.out.println("Do you want to add more meals? (yes/no)");
+                String moreMeals = scanner.next();
+                if (moreMeals.equalsIgnoreCase("no")){
+                    break mealsLoop;
+                } else if (moreMeals.equalsIgnoreCase("yes")){
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter 'yes' or 'no'.");
+                }
+            }
         }
-        
-
-
+        BillingService billingService = new BillingService();
+        // Discounts
+        Discount pizzaDiscount = new PizzaDiscount();
+        Discount meatDiscount = new MeatDiscount();
+        Discount chicketDiscount = new ChickenDiscount();
+        billingService.addDiscount(pizzaDiscount);
+        billingService.addDiscount(meatDiscount);
+        billingService.addDiscount(chicketDiscount);
+        // Creating bill
+        Bill bill = billingService.createBill(order);
+        System.out.println(bill.toString());
+        System.out.println("Please select payment method (1: Credit Card, 2: Mobile Wallet, 3: Cash): ");
+        Integer paymentMethod = scanner.nextInt();
+        PaymentMethod paymentStrategy = null;
+        switch (paymentMethod) {
+            case 1:
+                System.out.print("Enter credit card number: ");
+                String cardNumber = scanner.nextLine();
+                paymentStrategy = new CreditPayment(cardNumber);
+                break;
+            case 2:
+                System.out.print("Enter mobile number: ");
+                String mobileNumber = scanner.nextLine();
+                paymentStrategy = new MobileWalletPayment(mobileNumber);
+                break;
+            case 3:
+                paymentStrategy = new CashPayment();
+                break;
+            default:
+                System.out.println("Invalid payment method. Defaulting to Cash.");
+                paymentStrategy = new CashPayment();
+                break;
+        }
+        billingService.payBill(bill, paymentStrategy);
     }
 }
