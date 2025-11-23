f51737f232fb14e43c06645b1507b0f8aec265d9
Ahmed Mostafa <01029943015a@gmail.com>
2025-11-23 22:11:22 +0200

    u

--- pizzana/Pizzana.java
+++ pizzana/Pizzana.java
@@ -6,30 +6,36 @@ import java.util.Scanner;
 
 import pizzana.bills.*;
 import pizzana.discounts.*;
+import pizzana.meals.Meal;
 import pizzana.observers.*;
 import pizzana.payments.*;
 
 public class Pizzana {
 
     public static void main(String[] args) {
+        ArrayList<Meal> burgers = new ArrayList<Meal>();
+        ArrayList<Meal> pizzas = new ArrayList<Meal>();
         System.out.println("Welcome to Pizzana!");
         Scanner scanner = new Scanner(System.in);
 
         MenuDisplaySystem menuDisplay = new MenuDisplaySystem();
-        menuDisplay.showMainMenu();
+        menuDisplay.showMainMenu(burgers, pizzas);
         System.out.print("Please select your meal options: ");
         Integer mealOptions = scanner.nextInt();
         ArrayList<OrderItem> orderItems = new ArrayList<>();
 
         while (true)
         {
-            System.out.print("Enter meal name to add to order (or type 'done' to finish): ");
-            System.out.println("DO u want to add any extra ?");
-            scanner.
-            scanner.close();
+            System.out.print("Enter meal name (or 'done' to finish): ");
+            String mealName = scanner.nextLine();
+            if (mealName.equalsIgnoreCase("done")) {
+                break;
+            }
+            System.out.print("Enter quantity: ");
+            Integer quantity = scanner.nextInt();
+
+            System.out
         }
         
-
-
     }
 }
